b0d22d72e2bd4a8a358a2e7d2db0c6b439a7e45f
CyrilSmed <cyrilsmed@outlook.com>
2021-11-09 14:09:27 +0500

    Create Vector3D class base

--- oop-lab2.cpp
+++ oop-lab2.cpp
@@ -3,7 +3,51 @@
 
 #include <iostream>
 
+class Vector3D
+{
+private:
+    float x;
+    float y;
+    float z;
+
+public:
+    Vector3D()
+    {
+        std::cout << "Debug: call of default constructor\n";
+        x = y = z = 0;
+    }
+    Vector3D(int x, int y, int z)
+    {
+        std::cout << "Debug: call of constructor with parameters\n";
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+    Vector3D(const Vector3D& vector)
+    {
+        std::cout << "Debug: call of copy constructor\n";
+        x = vector.x;
+        y = vector.y;
+        z = vector.z;
+    }
+    ~Vector3D() 
+    {
+        std::cout << "Debug: call of destructor\n";
+    }
+
+    float dotProduct(const Vector3D* vector)
+    {
+        float product = 0; // TODO
+        return product;
+    }
+    float crossProduct(const Vector3D* vector)
+    {
+        float product = 0; // TODO
+        return product;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
