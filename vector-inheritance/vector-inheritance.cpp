4e8c92c359259f8790a4bae3c20f9d5507e1a6e9
CyrilSmed <cyrilsmed@outlook.com>
2021-11-10 18:03:21 +0500

    Add LabeledVector3D inherited from Vector3D

--- vector-inheritance/vector-inheritance.cpp
+++ vector-inheritance/vector-inheritance.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Vector
 {
 public:
-    virtual void printDescription() 
+    virtual void printVectorDescription() 
     {
         printf("Debug: Vector::printDescription()\n");
     }
@@ -75,7 +75,7 @@ public:
         return z;
     }
 
-    void printDescription()
+    void printVectorDescription()
     {
         printf("(%d, %d, %d)\n", x, y, z);
         printf("Debug: Vector3D::printDescription()\n");
@@ -99,29 +99,58 @@ Vector3D* Vector3D::crossProduct(const Vector3D* vector)
     return product;
 }
 
+class LabeledVector3D : public Vector3D
+{
+private:
+    string label = "";
+public:
+    LabeledVector3D() : Vector3D()
+    {
+        printf("Debug: LabeledVector3D - default constructor\n");
+        x = y = z = 0;
+    }
+    LabeledVector3D(int x, int y, int z, string label) : Vector3D(x, y, z)
+    {
+        printf("Debug: LabeledVector3D - constructor with parameters\n");
+        this->label = label;
+    }
+    LabeledVector3D(const LabeledVector3D& vector) : Vector3D(vector)
+    {
+        std::cout << "LabeledVector3D: Vector - copy constructor\n";
+        label = vector.label;
+    }
+    ~LabeledVector3D()
+    {
+        printf("Debug: Vector - destructor\n");
+    }
+    void printVectorDescription()
+    {
+        cout << label << ": ";
+        printf("(%d, %d, %d)\n", x, y, z);
+        printf("Debug: LabeledVector3D::printDescription()\n");
+    }
+
+    string getLabel() const
+    {
+        return label;
+    }
+};
+
 int main()
 {
-    Vector3D* a = new Vector3D(1, 3, 2);
-    Vector3D* b = new Vector3D(*a);
-    b->setY(-1);
-    printf("\n");
+    Vector* vectors[3] = {
+        new Vector3D(-1, 3, 0),
+        new LabeledVector3D(2, 6, 4, "Even Vector"),
+        new Vector3D()
+    };
 
-    printf("Vector a: ");
-    a->printDescription();
-    printf("\n");
-    printf("Vector b: ");
-    b->printDescription();
-    printf("\n\n");
+    for (int i = 0; i < sizeof(vectors) / sizeof(vectors[0]); i++)
+    {
+        vectors[i]->printVectorDescription();
+    }
 
-    int dotProduct = a->dotProduct(b);
-    printf("Dot product of vectors a and b: %d\n\n", dotProduct);
-
-    Vector3D* crossProduct = a->crossProduct(b);
-    printf("Cross product of vectors a and b: ");
-    crossProduct->printDescription();
-    printf("\n\n");
-
-    delete a;
-    delete b;
-    delete crossProduct;
+    for (int i = 0; i < sizeof(vectors) / sizeof(vectors[0]); i++)
+    {
+        delete vectors[i];
+    }
 }
